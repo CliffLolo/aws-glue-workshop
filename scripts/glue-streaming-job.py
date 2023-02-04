190cd67affde9ca542bd4ec6fb6db3c90eed6ded
Clifford Frempong <cliffordfrimpong69@gmail.com>
2023-02-04 22:12:11 +0000

    made changes to streaming script

--- scripts/glue-streaming-job.py
+++ scripts/glue-streaming-job.py
@@ -16,3 +16,10 @@ s3_bucket = "s3://${BUCKET_NAME}"
 output_path = s3_bucket + "/output/lab4/notebook/"
 job_time_string = datetime.now().strftime("%Y%m%d%H%M%S")
 s3_target = output_path + job_time_string
+
+country_lookup_frame = glueContext.create_dynamic_frame.from_options(
+                            format_options = {"withHeader":True, "separator":',', "quoteChar":"\""},
+                            connection_type = "s3",
+                            format = "csv",
+                            connection_options = {"paths": [s3_bucket + "/input/lab4/country_lookup/"], "recurse":True},
+                            transformation_ctx = "country_lookup_frame")
